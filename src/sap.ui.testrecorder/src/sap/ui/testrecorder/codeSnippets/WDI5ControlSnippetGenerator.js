51848c4a926f2c0f619dca0b480404ac94e70cfa
scoen <simon.coen@js-soft.com>
2022-04-11 14:23:37 +0200

    [FEATURE] Testrecorder: Add WDI5 Dialect
    - add wdi5 snippet generator
    - adapt POMethod generation as WDI5 requires async functions

---
+++ src/sap.ui.testrecorder/src/sap/ui/testrecorder/codeSnippets/WDI5ControlSnippetGenerator.js
@@ -0,0 +1,91 @@
+/*!
+ * ${copyright}
+ */
+
+sap.ui.define(
+    [
+        "sap/ui/testrecorder/codeSnippets/ControlSnippetGenerator",
+        "sap/ui/testrecorder/interaction/Commands"
+    ],
+    function (ControlSnippetGenerator, Commands) {
+        "use strict";
+
+        /**
+         * @class  generates a code snippet relevant to UIVeri5
+         */
+        var WDI5ControlSnippetGenerator = ControlSnippetGenerator.extend(
+            "sap.ui.testrecorder.codeSnippets.WDI5ControlSnippetGenerator",
+            {}
+        );
+
+        /**
+         * @param {object} mData data from which to generate a snippet
+         * @param {object} mData.controlSelector control selector in string format
+         * @param {string} mData.action name of the action to record for the control
+         * @param {object} mData.assertion assertion details - property name, type and expected value
+         * @returns {string} a stringified code snippet
+         */
+        WDI5ControlSnippetGenerator.prototype._generate = function (mData) {
+            var sBasicSelector =
+                "await browser.asControl(selector: " +
+                this._getSelectorAsString(mData.controlSelector) +
+                "))";
+            var sSelectorWithAssertion = this._getSelectorWithAssertion(
+                sBasicSelector,
+                mData.assertion
+            );
+            return (
+                sSelectorWithAssertion +
+                this._getActionAsString(mData.action) +
+                ";"
+            );
+        };
+
+        WDI5ControlSnippetGenerator.prototype._getActionAsString = function (
+            sAction
+        ) {
+            switch (sAction) {
+                case Commands.PRESS:
+                    return ".press()";
+                case Commands.ENTER_TEXT:
+                    return '.enterText("test")';
+                default:
+                    return "";
+            }
+        };
+
+        WDI5ControlSnippetGenerator.prototype._getSelectorWithAssertion =
+            function (sSelector, mAssertion) {
+                if (mAssertion) {
+                    var sMatcher;
+                    if (
+                        !mAssertion.expectedValue ||
+                        mAssertion.expectedValue === "false"
+                    ) {
+                        sMatcher = ".toBeFalsy()";
+                    } else if (mAssertion.propertyType === "boolean") {
+                        sMatcher = ".toBeTruthy()";
+                    } else {
+                        var sExpectedValue = this._escapeQuotes(
+                            mAssertion.expectedValue
+                        );
+                        sMatcher = '.toEqual("' + sExpectedValue + '")';
+                    }
+                    var sPropertyNameWithFirstLetterUpperCase =
+                        mAssertion.propertyName[0].toUpperCase() +
+                        mAssertion.propertyName.slice(1);
+                    return (
+                        sSelector +
+                        ".get" +
+                        sPropertyNameWithFirstLetterUpperCase +
+                        "()" +
+                        sMatcher
+                    );
+                } else {
+                    return sSelector;
+                }
+            };
+
+        return new WDI5ControlSnippetGenerator();
+    }
+);
